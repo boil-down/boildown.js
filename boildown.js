43c1678a9a29dd0c45dcae772271b1e6dc9f257d
Jan Bernitt <jaanbernitt@gmail.com>
2016-03-23 10:30:55 +0100

    code cleanup and small fixes in the details

--- boildown.js
+++ boildown.js
@@ -20,6 +20,7 @@ var Boildown = (function() {
 		["<em>$1</em>",     /_(..*?)_/g ],
 		["<span style='color: $1$2;'>$3</span>", /#(?:#([a-z]{1,10})|(#[0-9A-Fa-f]{6}))#(..*?)##/g ],
 		["<a href=\"$1\">$2</a>", /\[\[((?:https?:\/\/)?(?:[-_a-zA-Z0-9]{0,15}[.:/#+]?){1,20}) (.+?)\]\]/g ],
+		["$1<a href=\"$2$3\">$3</a>", /(^|[^=">])(https?:\/\/|www\.)((?:[-_a-zA-Z0-9]{0,15}[.:/#+]?){1,20})/g ],
 		["<a href=\"#sec-$1\">$1</a>", /\[\[(\d+(?:\.\d+)*)\]\]/g ],
 		["<sup><a href='#fnote$1'>$1</a></sup>", /\^\[(\d+)\]/g ],
 	];
@@ -42,7 +43,7 @@ var Boildown = (function() {
 		[ bParagraph, /^(.|$)/ ]
 	];
 
-	const OPTIONS = [
+	const STYLES = [
 		// dynamic values
 		[ /^(?: ?[a-zA-Z][-_a-zA-Z0-9]*)+$/ ], // length 1 => classes, else => style
 		[ /^[0-9]{1,3}%$/, "width"],
@@ -65,28 +66,21 @@ var Boildown = (function() {
 	];
 
 	function Doc(markup) {
-		this.markup = markup;		
+		this.markup = markup;
 		this.lines  = markup.split(/\r?\n/g);
 		this.levels = [0,0,0,0,0,0,0];
 		this.html   = "";
+		// common functions
+		this.process  = process;
+		this.styles   = styles;
+		this.scan     = scan;
+		this.unindent = unindent;
 	}
 
 	var _ = Doc.prototype; 
 	_.line       = function (n) { return this.lines[n]; }
-	_.deItem     = function (n) { var l = this.lines[n]; this.lines[n] = l.substring(l.indexOf(' ')); }
-	_.unblock    = function (n, e, regex) { while (n < e && regex.test(this.lines[n])) { this.lines[n] = this.lines[n++].substring(2); } return n; }
 	_.matches    = function (n, regex) { return regex.test(this.lines[n]); }
 	_.add        = function (elem) { this.html+=elem; }
-	_.process    = function (start, end, level) {
-		var i = start;
-		while (i < end) {
-			var j = i;
-			var block = first(BLOCKS, this.line(i), 1);
-			i = block[0](this, i, end, level, block[1]);
-			if (i == j) { console.log("endless loop at line: "+i); i++; } // inc to go on...
-			j = i;
-		}
-	}
 
 	return {
 		toHTML: processMarkup
@@ -98,10 +92,40 @@ var Boildown = (function() {
 		return doc.html;
 	}
 
+	/* common functions on documents - must be called so this is the document */
+
+	function process(start, end, level) {
+		var i = start;
+		while (i < end) {
+			var before = i;
+			var block = first(BLOCKS, this.lines[i], 1);
+			i = block[0](this, i, end, level, block[1]);
+			if (i === before) { 
+				if (console && console.log) { console.log("endless loop at line "+i+": "+this.lines[i]); }
+				i++; // error recovery: go on on next line
+			} 
+		}
+	}
+
+	function unindent(width, start, end, pattern) {
+		var i = start;
+		while (i < end && pattern.test(this.lines[i])) {
+			this.lines[i] = this.lines[i].substring(Math.min(this.lines[i].length, width));
+			i++;
+		} 
+		return i;
+	}
+
+	function scan(start, end, pattern) {
+		var i = start;
+		while (i < end && !pattern.test(this.lines[i])) { i++; }
+		return i;
+	}
+
 	// BLOCK functions return the line index after the block
 
 	function bLine(doc, start, end, level) {
-		doc.add("<hr "+processOptions(doc.line(start))+" />\n");
+		doc.add("<hr "+doc.styles(doc.line(start))+" />\n");
 		return start+1;
 	}
 
@@ -126,12 +150,12 @@ var Boildown = (function() {
 		if (start > 0 && pattern.test(doc.line(start-1))) { n++; }
 		doc.levels[n]++;
 		var parts = pattern.exec(doc.line(start));
-		doc.add("<h"+n+processOptions(parts[2])+">"+processLine(parts[1])+"</h"+n+">\t");
+		doc.add("<h"+n+" "+doc.styles(parts[2])+">"+processLine(parts[1])+"</h"+n+">\t");
 		return start+1;
 	}
 
 	function bNQuote(doc, start, end, level, pattern) {
-		var i = doc.unblock(start, end, pattern);
+		var i = doc.unindent(2, start, end, pattern);
 		doc.add("<blockquote>\n"); 
 		doc.process(start, i, level+1);
 		doc.add("</blockquote>\n");
@@ -139,34 +163,31 @@ var Boildown = (function() {
 	}
 
 	function bQuote(doc, start, end, level, pattern) {
-		var i = start+1;
-		while (i < end && !pattern.test(doc.line(i))) { i++; }		
-		doc.add("<blockquote "+processOptions(doc.line(start))+">\n"); 
+		var i = doc.scan(start+1, end, pattern);
+		doc.add("<blockquote "+doc.styles(doc.line(start))+">\n"); 
 		doc.process(start+1, i, level+1);
 		doc.add("</blockquote>\n");
 		return i+1;
 	}
 
 	function bDeleted(doc, start, end, level, pattern) {
-		var i = start+1;
-		while (i < end && !pattern.test(doc.line(i))) { i++; }
-		doc.add("<del "+processOptions(doc.line(start))+">\n"); 
+		var i = doc.scan(start+1, end, pattern);
+		doc.add("<del "+doc.styles(doc.line(start))+">\n"); 
 		doc.process(start+1, i, level);
 		doc.add("</del>\n");		
 		return i+1;
 	}
 
 	function bInserted(doc, start, end, level, pattern) {
-		var i = start+1;
-		while (i < end && !pattern.test(doc.line(i))) { i++; }
-		doc.add("<ins "+processOptions(doc.line(start))+">\n"); 
+		var i = doc.scan(start+1, end, pattern);
+		doc.add("<ins "+doc.styles(doc.line(start))+">\n"); 
 		doc.process(start+1, i, level);
 		doc.add("</ins>\n");		
 		return i+1;
 	}
 
 	function bDescribe(doc, start, end, level, pattern) {
-		var i = doc.unblock(start, end, pattern);
+		var i = doc.unindent(2, start, end, pattern);
 		doc.add("<table class='describe'><tr><td><pre class='source'>");
 		var l0 = doc.html.length;
 		// following line must be done before processing the lines!
@@ -182,7 +203,7 @@ var Boildown = (function() {
 	}
 
 	function bExample(doc, start, end, level, pattern) {
-		var i = doc.unblock(start, end, pattern);
+		var i = doc.unindent(2, start, end, pattern);
 		doc.add("<table class='example'><tr><td><pre class='source'>");
 		var l0 = doc.html.length;
 		// following line must be done before processing the lines!
@@ -201,7 +222,7 @@ var Boildown = (function() {
 		//TODO highlighting of keywords
 		var i = start;
 		var ci = 16;
-		doc.add("<pre"+processOptions(doc.line(i++))+">\n");
+		doc.add("<pre"+doc.styles(doc.line(i++))+">\n");
 		while (i < end && !pattern.test(doc.line(i))) { 
 			var line = doc.line(i++);
 			if (line.charAt(0) === '!')  {
@@ -234,7 +255,7 @@ var Boildown = (function() {
 		var firstRow = true;
 		while (i < end && pattern.test(doc.line(i))) { 
 			var line = doc.line(i);
-			if (i == start) { doc.add("<table "+processOptions(line.startsWith("|[") ? line : "", "user")+">"); }
+			if (i == start) { doc.add("<table "+doc.styles(line.startsWith("|[") ? line : "", "user")+">"); }
 			if (doc.matches(i, /\|\[ /)) {
 				var side = i === start ? "top" : "bottom";
 				var caption = line.substring(line.indexOf(" "), line.indexOf("]|"));
@@ -247,7 +268,7 @@ var Boildown = (function() {
 				firstRow = false;
 				var classes = line.indexOf('=') < 0 ? "" : "tborder";
 				//TODO end table with border doesn't work as the tr is empty
-				doc.add("<tr "+processOptions(doc.line(i), classes)+">");
+				doc.add("<tr "+doc.styles(doc.line(i), classes)+">");
 			} else if (doc.matches(i, /\|+!? /)) {
 				//TODO col and rowspan (needed as there is no fallback language!)
 				if (firstRow) { doc.add("<tr>"); }
@@ -257,7 +278,7 @@ var Boildown = (function() {
 				var eom = line.lastIndexOf("|");
 				var som = line.lastIndexOf(" ", eom);
 				var classes = (line.indexOf("||") === 0 ? " lborder" : "")+(line.indexOf("||", som) >= som ? " rborder" : "");
-				doc.add("<"+tag+" "+processOptions(line.substring(eom), classes)+">");
+				doc.add("<"+tag+" "+doc.styles(line.substring(eom), classes)+">");
 				doc.add(processLine(line.substring(line.indexOf(" "), som)));
 				doc.add("</"+tag+">");
 			}
@@ -275,8 +296,8 @@ var Boildown = (function() {
 		var item = true;
 		while (i < end && pattern.test(doc.line(i))) {
 			var i0 = i;
-			doc.deItem(i);
-			i = doc.unblock(i+1, end, /^([ \t]{2}|\\s*$)/);
+			doc.lines[i] = doc.lines[i].substring(doc.lines[i].indexOf(' '));
+			i = doc.unindent(2, i+1, end, /^(?:(?:[ \t]{2})|\s*$)/);
 			doc.add("<li>\n\t");
 			doc.process(i0, i, level);
 			doc.add("</li>\n");
@@ -291,16 +312,14 @@ var Boildown = (function() {
 	function bMinipage(doc, start, end, level) {
 		var line = doc.line(start);
 		var depth = line.search(/[^:]/);
-		var eop = new RegExp("^[:]{"+depth+"}($|[^:])");
-		var i = start+1;
-		while (i < end && !eop.test(doc.line(i))) { i++; }
-		doc.add("<div"+processOptions(line, "bd-mp")+">\n\t");		
+		var i = doc.scan(start+1, end, new RegExp("^[:]{"+depth+"}($|[^:])"));
+		doc.add("<div"+doc.styles(line, "bd-mp")+">\n\t");		
 		doc.process(start+1, i, level+1); 
 		doc.add("</div>\n");
 		return i+1;
 	}
 
-	function processOptions(line, classes) {
+	function styles(line, classes) {
 		classes = classes ? classes : "";
 		line = line ? line : "";
 		var styles = "";
@@ -308,15 +327,12 @@ var Boildown = (function() {
 		while (start >= 0) {
 			var end = line.indexOf(']', start);
 			var val = line.substring(start+1, end);
-			var option = first(OPTIONS, val, 0);
-			if (option) {
-				if (option.length === 1) {
-					classes+=" "+val;
-				} else {
-					styles+=" "+option[1]+":"+(option.length > 2 ? option[2] : val)+";";
-				}
-				if (option.length > 3) {
-					classes+=" "+option[3];
+			var style = first(STYLES, val, 0);
+			if (style) {
+				switch (style.length) {
+					case 1:	classes+=" "+val; break;
+					case 3: classes+=" "+style[3]; /* intentianal fall-through */
+					case 2: styles+=" "+style[1]+":"+(style.length > 2 ? style[2] : val)+";";
 				}
 			}
 			start = line.indexOf('[', end);
